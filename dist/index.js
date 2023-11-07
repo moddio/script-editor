6f170a219405794269aa2e93909984169e0d3722
moe-moe-pupil <dhtmitmi@gmail.com>
2023-11-07 21:19:43 +0800

    chore: remove dist folder content

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import TextScriptEditor from "./components/TextScriptEditor";
-// import './index.css'
-// import 'bootstrap/dist/css/bootstrap.css';
-export default TextScriptEditor;
--- dist/stories/MultiElements.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import TextScriptEditor from "../components/TextScriptEditor";
-export const CombinedComp = () => {
-    return (_jsxs("div", { children: [_jsx(TextScriptEditor, { debug: true }), _jsx(TextScriptEditor, { debug: true })] }));
-};
