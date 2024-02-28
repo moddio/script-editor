6e38b3f23f53936905650e22bb1a6fc49fd2f86d
MoeThun <dhtmitmi@gmail.com>
2024-02-29 00:45:50 +0800

    chore: export TextScriptEditorMultiline

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { aliasTable, actionToString } from "script-parser"
 import { removeUnusedProperties } from "./utils/obj"
 import TextScriptEditor from "./components/TextScriptEditor"
+import TextScriptEditorMultiline from './components/TextScriptEditorMultiline'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export { TextScriptEditor, aliasTable, removeUnusedProperties, actionToString }
+export { TextScriptEditor, aliasTable, removeUnusedProperties, actionToString, TextScriptEditorMultiline }
