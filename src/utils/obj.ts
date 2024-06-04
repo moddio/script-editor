e81ab190eb27910cc91df6df3b9577a991d735c2
MoeThun <dhtmitmi@gmail.com>
2024-06-05 00:45:36 +0800

    fix: obj maybe is null in removeUnusedProperties

--- src/utils/obj.ts
+++ src/utils/obj.ts
@@ -1,7 +1,7 @@
 import { ExtraDataProps } from "../components/TextScriptEditor"
 
 export const removeUnusedProperties = (obj: { [key: string]: any }, extraData: ExtraDataProps, effects?: ((k: string, v: any, extraData: ExtraDataProps) => void)[]): { [P in keyof typeof obj as P extends string ? P extends `_${infer _}` ? never : P : never]: typeof obj[P] } => {
-  if(Array.isArray(obj)) { return obj }
+  if (obj === undefined || obj === null || typeof obj !== 'object' || Array.isArray(obj)) { return obj }
   effects?.forEach((effect) => { effect('', obj, extraData) })
   const newObj: { [P in keyof typeof obj as P extends string ? P extends `_${infer _}` ? never : P : never]: typeof obj[P] } = {}
   Object.keys(obj).forEach(key => {
